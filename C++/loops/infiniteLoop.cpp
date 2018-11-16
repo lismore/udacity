3198a39a9f6929ee998b74be13f133d292c14963
Patrick Lismore <patrick@patricklismore.com>
2018-11-16 17:35:55 +0000

    added infiniteLoop for C++

---
+++ C++/loops/infiniteLoop.cpp
@@ -0,0 +1,17 @@
+/**
+ * Infinite Loop Example - useful for embedded systems
+ * To compile: $ g++ -o infiniteLoop infiniteLoop.cpp
+ * To execute: $ ./infiniteLoop 
+ * */
+
+#include <iostream>
+
+int main()
+{
+    for(; ;){
+
+        std::cout<<"This for loop will run forever\n";
+    }
+
+    return 0;
+}
